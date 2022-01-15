3ad2dc91b817af5647b8c34cab42a94c0a02c009
Sven-27 <sven.notermans@live.nl>
2022-01-15 21:59:14 +0100

    creating data object

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import './App.css';
+import data from './data/data'
 
 function App() {
   return (
     <div className="app">
-      <h1>Start of travel journal</h1>
+      {
+        data.map(item => (
+          <img key={item.id} src={item.img} alt="images" />
+        ))
+      }
     </div>
   );
 }
---
+++ src/data/data.js
@@ -0,0 +1,20 @@
+import fuji from '../img/fuji.jpg'
+import fjord from '../img/geirangerfjord.jpg'
+import opera from '../img/opera-house.webp'
+
+const data = [
+  {
+    id: 1,
+    img: fuji,
+  },
+  {
+    id: 2,
+    img: fjord,
+  },
+  {
+    id: 3,
+    img: opera,
+  }
+]
+
+export default data
