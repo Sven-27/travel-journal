7c79c5e6999e02005113b74550548ea608c4b1a2
Sven-27 <sven.notermans@live.nl>
2022-01-16 14:20:53 +0100

    made objects of countrys

--- src/data/data.js
+++ src/data/data.js
@@ -6,15 +6,29 @@ const data = [
   {
     id: 1,
     img: fuji,
-  },
-  {
-    id: 2,
-    img: fjord,
+    country: 'Japan',
+    date: '12 Jan, 2021 - 24 Jan, 2021',
+    description: `Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters
+    (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for 
+    both Japanese and foreign tourists.`
   },
   {
     id: 3,
     img: opera,
-  }
+    country: 'Australia',
+    date: '27 May, 2021 - 8 Jun, 2021',
+    description: `The Sydney Opera House is a multi-venue performing arts centre
+    in Sydney. Located on the banks of the Sydney Harbour, it is often
+    regarded as one of the 20th century's most and distinctive buildings.`
+  },
+  {
+    id: 2,
+    img: fjord,
+    country: 'Norway',
+    date: '01 Oct, 2021 - 18 Nov, 2021',
+    description: `The Geiranger Fjord is a fjord in the Sunnmøre of Møre og
+    Romsdal country, Norway. It is located entirely in the Stranda Municipality.`
+  }  
 ]
 
 export default data
